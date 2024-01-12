e3982e0004af3ecd2d8fcf0b50aaaa8f11df4942
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-01-12 22:12:57 +0300

    Calling the component on the entry page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import LoginSignup from './Components/LoginSignup/LoginSignup';
+import React from 'react';
+import PasswordToggle from './Components/LoginSignup/LoginSignup';
 
-//import user_icon from '../Assets/person.png';
-import email_icon from './Components/Assets/email.png';
-import password_icon from './Components/Assets/password.png';
-
-function App() {
+const App = () => {
   return (
-    <div className='container'>
-    <div className='header'>
-    <div className='text'> Login </div>
-    <div className='underline'></div>
-    </div>
-    <div className='inputs'>
-    <div className='input'>
-    <img src={email_icon} alt='' />
-    <input type='email' />
-    </div>
-    <div className='input'>
-    <img src={password_icon} alt='' />
-    <input type='password' />
-    </div>
-    </div>
+    <div>
+      <h1>React Password Toggle Example</h1>
+      <PasswordToggle />
     </div>
   );
-}
+};
 
 export default App;
